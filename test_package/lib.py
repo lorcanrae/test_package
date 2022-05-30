f7c2e1ce6de4421d2ee2f40197ebc058d8f95226
Lorcan Rae <lorcanrae@outlook.com>
2022-05-30 14:20:27 +0100

    fixed function

--- test_package/lib.py
+++ test_package/lib.py
@@ -1,2 +1,3 @@
 def try_me():
-    return 'I think you are wonderful and are doing great, keep it up!\nSmall steps and deep breaths.'
+    while True:
+        print('I think you are wonderful and are doing great, keep it up!\nSmall steps and deep breaths.')
